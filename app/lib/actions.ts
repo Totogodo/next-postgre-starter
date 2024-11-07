d5c350e988a8ceab4a54693064c295606c7d8883
Mykola Zaiarnyi <mzaiarnyi@gmail.com>
2024-11-07 22:55:38 +0100

    feat: add update invoice page & validate data & update data function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,14 @@
 "use server";
-
+// Zod for data validation
 import z from "zod";
+// SQL to make sql query to the server
+import { sql } from "@vercel/postgres";
+// revalidate path to update cache so you can see relevant data on the page
+import { revalidatePath } from "next/cache";
+// redirect function
+import { redirect } from "next/navigation";
 
+// create form data schema to check how data should look like
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -20,4 +27,34 @@ export async function createInvoice(formData: FormData) {
   });
 
   const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+  // Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+const UpdateInvoice = FormSchema.omit({id: true, date: true})
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      });
+     
+      const amountInCents = amount * 100;
+     
+      await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+     
+      revalidatePath('/dashboard/invoices');
+      redirect('/dashboard/invoices');
 }
